007afbe984aec352816c7dfe452f24f8d381f835
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 14:57:26 +0300

    Stop rook from moving though units

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -11,11 +11,32 @@ export default class Rook extends Piece {
     public getAvailableMoves(board: Board) {
         let currentPosition=board.findPiece(this);
         let arrayOfMoves = new Array();
-        for (let i=0;i<=7;i++)
+        for (let i=currentPosition.row-1;i>=0;i--)
         {
-            if (i!=currentPosition.row)
+            if (board.getPiece(new Square(i,currentPosition.col))!=undefined)
+                break;
+            else
                 arrayOfMoves.push(new Square(i,currentPosition.col));
-            if (i!=currentPosition.col)
+        }
+        for (let i=currentPosition.row+1;i<=7;i++)
+        {
+            if (board.getPiece(new Square(i,currentPosition.col))!=undefined)
+                break;
+            else
+                arrayOfMoves.push(new Square(i,currentPosition.col));
+        }
+        for (let i=currentPosition.col-1;i>=0;i--)
+        {
+            if (board.getPiece(new Square(currentPosition.row,i))!=undefined)
+                break;
+            else
+                arrayOfMoves.push(new Square(currentPosition.row,i));
+        }
+        for (let i=currentPosition.col+1;i<=7;i++)
+        {
+            if (board.getPiece(new Square(currentPosition.row,i))!=undefined)
+                break;
+            else
                 arrayOfMoves.push(new Square(currentPosition.row,i));
         }
         return arrayOfMoves;
