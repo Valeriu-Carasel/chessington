fd483bbea77166f0b20db06a56b2655ede87acf7
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 16:21:58 +0300

    Abia la cal am realizat ca dau copy paste la aceasi for si probabil ar trebui o functie separata

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import CheckBounds from "./CheckBounds";
 import Square from "../square";
+import King from "./king";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -20,6 +21,16 @@ export default class Knight extends Piece {
                 if (CheckBounds.inBounds(currentPosition.col + j) && CheckBounds.inBounds(currentPosition.row + i)) {
                     arrayOfMoves.push(new Square(currentPosition.row + j, currentPosition.col + i));
                 }
+            }
+        }
+        for (let i=0;i<arrayOfMoves.length;i++) {
+            if (board.getPiece(arrayOfMoves.at(i))?.player==this.player) {
+                arrayOfMoves.splice(i,1);
+            }
+            else
+            {
+                if (board.getPiece(arrayOfMoves.at(i)) instanceof King)
+                    arrayOfMoves.splice(i,1);
             }
         }
         return arrayOfMoves;
