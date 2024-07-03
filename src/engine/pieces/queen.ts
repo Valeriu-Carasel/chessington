52e4270e76a67d40cdd55fffb4c1bf5ae8d0d37e
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 15:46:22 +0300

    Stop queen from moving through units

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -13,21 +13,67 @@ export default class Queen extends Piece {
     public getAvailableMoves(board: Board) {
         let currentPosition=board.findPiece(this);
         let arrayOfMoves=new Array();
-        for (let i=-7;i<=7;i++)
+
+        //rook part
+        for (let i=currentPosition.row-1;i>=0;i--)
         {
-            if (i!=0) {
-                if (CheckBounds.inBounds(currentPosition.row + i) && CheckBounds.inBounds(currentPosition.col + i))
-                    arrayOfMoves.push(new Square(currentPosition.row + i, currentPosition.col + i));
-                if (CheckBounds.inBounds(currentPosition.row - i) && CheckBounds.inBounds(currentPosition.col + i))
-                    arrayOfMoves.push(new Square(currentPosition.row - i, currentPosition.col + i));
-            }
-        }
-        for (let i=0;i<=7;i++)
-        {
-            if (i!=currentPosition.row)
+            if (board.getPiece(new Square(i,currentPosition.col))!=undefined)
+                break;
+            else
                 arrayOfMoves.push(new Square(i,currentPosition.col));
-            if (i!=currentPosition.col)
+        }
+        for (let i=currentPosition.row+1;i<=7;i++)
+        {
+            if (board.getPiece(new Square(i,currentPosition.col))!=undefined)
+                break;
+            else
+                arrayOfMoves.push(new Square(i,currentPosition.col));
+        }
+        for (let i=currentPosition.col-1;i>=0;i--)
+        {
+            if (board.getPiece(new Square(currentPosition.row,i))!=undefined)
+                break;
+            else
                 arrayOfMoves.push(new Square(currentPosition.row,i));
+        }
+        for (let i=currentPosition.col+1;i<=7;i++)
+        {
+            if (board.getPiece(new Square(currentPosition.row,i))!=undefined)
+                break;
+            else
+                arrayOfMoves.push(new Square(currentPosition.row,i));
+        }
+
+        //bishop part
+        let position:Square=new Square(currentPosition.row+1,currentPosition.col+1);
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        {
+            //intreaba-l pe Raimond, aici aveai position in loc de new Square si facea niste chestii ciudate
+            //was it a refference?
+            arrayOfMoves.push(new Square(position.row,position.col));
+            position.col+=1;
+            position.row+=1;
+        }
+        position=new Square(currentPosition.row-1,currentPosition.col-1);
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        {
+            arrayOfMoves.push(new Square(position.row,position.col));
+            position.col-=1;
+            position.row-=1;
+        }
+        position=new Square(currentPosition.row-1,currentPosition.col+1);
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        {
+            arrayOfMoves.push(new Square(position.row,position.col));
+            position.col+=1;
+            position.row-=1;
+        }
+        position=new Square(currentPosition.row+1,currentPosition.col-1);
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        {
+            arrayOfMoves.push(new Square(position.row,position.col));
+            position.col-=1;
+            position.row+=1;
         }
         return arrayOfMoves;
     }
