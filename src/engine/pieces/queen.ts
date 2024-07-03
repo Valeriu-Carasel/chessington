b9eb97430831ae388a4cfe3b82bfb824373061e6
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 16:20:51 +0300

    Queens can't take wrong pieces

--- src/engine/pieces/queen.ts
+++ src/engine/pieces/queen.ts
@@ -4,6 +4,7 @@ import Board from '../board';
 import Square from "../square";
 import Bishop from "./bishop";
 import CheckBounds from "./CheckBounds";
+import King from "./king";
 
 export default class Queen extends Piece {
     public constructor(player: Player) {
@@ -11,69 +12,83 @@ export default class Queen extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        let currentPosition=board.findPiece(this);
-        let arrayOfMoves=new Array();
-
         //rook part
+        let currentPosition=board.findPiece(this);
+        let arrayOfMoves = new Array();
         for (let i=currentPosition.row-1;i>=0;i--)
         {
+            arrayOfMoves.push(new Square(i,currentPosition.col));
             if (board.getPiece(new Square(i,currentPosition.col))!=undefined)
                 break;
-            else
-                arrayOfMoves.push(new Square(i,currentPosition.col));
         }
         for (let i=currentPosition.row+1;i<=7;i++)
         {
+            arrayOfMoves.push(new Square(i,currentPosition.col));
             if (board.getPiece(new Square(i,currentPosition.col))!=undefined)
                 break;
-            else
-                arrayOfMoves.push(new Square(i,currentPosition.col));
         }
         for (let i=currentPosition.col-1;i>=0;i--)
         {
+            arrayOfMoves.push(new Square(currentPosition.row,i));
             if (board.getPiece(new Square(currentPosition.row,i))!=undefined)
                 break;
-            else
-                arrayOfMoves.push(new Square(currentPosition.row,i));
         }
         for (let i=currentPosition.col+1;i<=7;i++)
         {
+            arrayOfMoves.push(new Square(currentPosition.row,i));
             if (board.getPiece(new Square(currentPosition.row,i))!=undefined)
                 break;
-            else
-                arrayOfMoves.push(new Square(currentPosition.row,i));
         }
 
         //bishop part
         let position:Square=new Square(currentPosition.row+1,currentPosition.col+1);
-        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col))
         {
             //intreaba-l pe Raimond, aici aveai position in loc de new Square si facea niste chestii ciudate
             //was it a refference?
             arrayOfMoves.push(new Square(position.row,position.col));
+            if (board.getPiece(position)!=undefined)
+                break;
             position.col+=1;
             position.row+=1;
         }
         position=new Square(currentPosition.row-1,currentPosition.col-1);
-        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col))
         {
             arrayOfMoves.push(new Square(position.row,position.col));
+            if (board.getPiece(position)!=undefined)
+                break;
             position.col-=1;
             position.row-=1;
         }
         position=new Square(currentPosition.row-1,currentPosition.col+1);
-        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col))
         {
             arrayOfMoves.push(new Square(position.row,position.col));
+            if (board.getPiece(position)!=undefined)
+                break;
             position.col+=1;
             position.row-=1;
         }
         position=new Square(currentPosition.row+1,currentPosition.col-1);
-        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col) && board.getPiece(position)==undefined)
+        while (CheckBounds.inBounds(position.row) && CheckBounds.inBounds(position.col))
         {
             arrayOfMoves.push(new Square(position.row,position.col));
+            if (board.getPiece(position)!=undefined)
+                break;
             position.col-=1;
             position.row+=1;
+        }
+        //removal part
+        for (let i=0;i<arrayOfMoves.length;i++) {
+            if (board.getPiece(arrayOfMoves.at(i))?.player==this.player) {
+                arrayOfMoves.splice(i,1);
+            }
+            else
+            {
+                if (board.getPiece(arrayOfMoves.at(i)) instanceof King)
+                    arrayOfMoves.splice(i,1);
+            }
         }
         return arrayOfMoves;
     }
