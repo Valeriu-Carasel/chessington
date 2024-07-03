920a771ba2220077a7b746a74919430bc5528e8d
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 13:32:05 +0300

    Made knight move

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import CheckBounds from "./CheckBounds";
+import Square from "../square";
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,18 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentPosition=board.findPiece(this);
+        let arrayOfMoves=new Array();
+        for (let i=-2;i<=2;i+=4) {
+            for (let j:number=-1;j<=1;j+=2) {
+                if (CheckBounds.inBounds(currentPosition.col + i) && CheckBounds.inBounds(currentPosition.row + j)) {
+                    arrayOfMoves.push(new Square(currentPosition.row + i, currentPosition.col + j));
+                }
+                if (CheckBounds.inBounds(currentPosition.col + j) && CheckBounds.inBounds(currentPosition.row + i)) {
+                    arrayOfMoves.push(new Square(currentPosition.row + j, currentPosition.col + i));
+                }
+            }
+        }
+        return arrayOfMoves;
     }
 }
