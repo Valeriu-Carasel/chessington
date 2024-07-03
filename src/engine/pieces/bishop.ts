fd9dc1f17879c9fafe2dcfc17de1eba16b21fac2
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 13:06:35 +0300

    Made bishop move diagonally

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,21 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let currentPosition=board.findPiece(this);
+        let arrayOfMoves = new Array();
+        for (let i=-7;i<=7;i++)
+        {
+            if (i!=0) {
+                if (this.inBounds(currentPosition.row + i) && this.inBounds(currentPosition.col + i))
+                    arrayOfMoves.push(new Square(currentPosition.row + i, currentPosition.col + i));
+                if (this.inBounds(currentPosition.row - i) && this.inBounds(currentPosition.col + i))
+                    arrayOfMoves.push(new Square(currentPosition.row - i, currentPosition.col + i));
+            }
+        }
+        return arrayOfMoves;
+    }
+
+    public inBounds(numar:number):boolean{
+        return numar >= 0 && numar <= 7;
     }
 }
