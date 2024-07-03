e205932a93865afd09fea7f210c756f625f392f3
Valeriu Carasel <valeriu.carasel@softwire.com>
2024-07-03 12:37:36 +0300

    Made pawns move...?

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from "../square";
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,13 @@ export default class Pawn extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+
+        let currentPlace=board.findPiece(this);
+        let arrayOfMoves=new Array();
+        if (this.player==Player.WHITE)
+            arrayOfMoves.push(new Square(currentPlace.row+1,currentPlace.col));
+        else
+            arrayOfMoves.push(new Square(currentPlace.row-1,currentPlace.col));
+        return arrayOfMoves;
     }
 }
